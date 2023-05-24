e89cdb920a87befc4e2af13cfd2a160c5a5c86d8
Jack Hagen <74005486+ThisIsNotANamepng@users.noreply.github.com>
2023-05-24 16:33:10 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,6 @@ print("Installing packages... ")
 os.system("pip install bs4")
 os.system("pip install nltk")
 os.system("pip install libzim")
+os.system("pip install scikit-learn")
+
 print("Packages installed!")
