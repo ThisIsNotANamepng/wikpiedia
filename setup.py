e0057c3254a2b9d8865bce227987f9e88f35147f
Jack Hagen <74005486+ThisIsNotANamepng@users.noreply.github.com>
2023-05-24 16:29:12 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ import os
 
 print("Installing packages... ")
 os.system("pip install bs4")
-os.system("pip install txtai")
+os.system("pip install nltk")
 os.system("pip install libzim")
 print("Packages installed!")
